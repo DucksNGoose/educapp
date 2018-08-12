2d0b91fafc0569c575f9b409144a560ea0b3df9f
Henrique Alves <henri.falves@hotmail.com>
2018-08-12 00:56:39 -0300

    retirado o comentário de código de step

--- front-end/educapp/App.js
+++ front-end/educapp/App.js
@@ -7,26 +7,3 @@ const NavApp = createStackNavigator({
 });
 
 export default NavApp; 
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//         <Text>Changes you make will automatically reload.</Text>
-//         <Text>Shake your phone to open the developer menu.</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
