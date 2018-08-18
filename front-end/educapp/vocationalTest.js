20138fe972251dae9f1c2ded180e7ab58a6f7aeb
Henrique Alves <henri.falves@hotmail.com>
2018-08-18 19:47:02 -0300

    (Evandro) - criação de um botão de inicio do teste

--- front-end/educapp/vocationalTest.js
+++ front-end/educapp/vocationalTest.js
@@ -5,12 +5,13 @@ export default class VocationalTest extends React.Component {
   static navigationOptions = {
     title: "EducApp - Learn U"
   }
-  
+
   render() {
     return (
       <View style={Viewstyles.container}>
         <Text style={textStyles.container}>EducApp - Um App Passatempo Educacional</Text>      
         <Text>Teste vocacional pra vc que não sabe oq fazer, seu imaturo</Text>
+        <Button onPress={()=>{}} title="Começar Teste"/>
       </View>
     );
   }
